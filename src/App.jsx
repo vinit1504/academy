5033de5fc5afca103ca2528b3c6decd6c4303508
vinit <vinit1222003@gmail.com>
2024-07-24 18:04:22 +0530

    add hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 /* eslint-disable no-unused-vars */
 import React from 'react'
-import Navbar from './components/Navbar';
+// import Navbar from './components/Navbar';
+import HeroSection from './components/HeroSection';
 
 const App = () => {
   return (
     <div>
-      <Navbar />
+      <HeroSection />
     </div>
   )
 }
