fabaa51320438d7b2b14480082e84e0083407f68
vinit <vinit1222003@gmail.com>
2024-07-24 16:44:41 +0530

    logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+/* eslint-disable no-unused-vars */
 import React from 'react'
+import Navbar from './components/Navbar';
 
 const App = () => {
   return (
     <div>
-      
+      <Navbar />
     </div>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,13 @@
+/* eslint-disable no-unused-vars */
 import React from 'react'
+import logo from './../assets/img/logo.svg'
 
 const Navbar = () => {
   return (
-    <div>
-      
+    <div className='navbar'>
+      <div className='logo'>
+        <img src={logo} alt="" />
+      </div>
     </div>
   )
 }
